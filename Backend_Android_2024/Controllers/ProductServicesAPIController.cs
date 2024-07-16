e4362ff1aba46caefc4a98516811349bc28f9470
huynhthienthe <huynhthienthetv2020@gmail.com>
2024-07-16 16:10:57 +0700

    Update: Prodcut service - Get: Card Item for Futter App

---
+++ Backend_Android_2024/Controllers/ProductServicesAPIController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Backend_Android_2024.Models;
+using Backend_Android_2024.Models.DTOModel;
+using Backend_Android_2024.Models.ServicesDTOModel.Backend_Android_2024.Models.DTOModel;
+
+namespace Backend_Android_2024.Controllers
+{
+    public class ProductServicesController : ApiController
+    {
+        private TestShoppingEntities db = new TestShoppingEntities();
+
+        /// <summary>
+        /// Get list Card Item for home page Flutter 
+        /// </summary>
+        /// <returns></returns>
+        // GET: api/ServicesAPI
+        public IQueryable<ProductCartItemDTO> GetProductCart()
+        {
+            var query = db.SanPhams
+                    .Select(sp => new ProductCartItemDTO
+                    {
+                        IDSP = sp.IDSP,
+                        TenSP = sp.TenSP,
+                        Img_Url = db.HinhAnhs
+                                    .Where(i => i.IDSP == sp.IDSP)
+                                    .Select(i => i.TenHinh)
+                                    .FirstOrDefault(),
+                        GiaBan = db.ChiTiet_SP
+                                    .Where(ct => ct.IDSP == sp.IDSP)
+                                    .Select(ct => ct.GiaBan)
+                                    .FirstOrDefault(),
+                    });
+
+            return query;
+        }
+
+        private bool SanPhamExists(int id)
+        {
+            return db.SanPhams.Count(e => e.IDSP == id) > 0;
+        }
+    }
+}
--- Backend_Android_2024/Models/ServicesDTOModel/ServicesDTO.cs
+++ Backend_Android_2024/Models/ServicesDTOModel/ServicesDTO.cs
@@ -9,35 +9,14 @@ namespace Backend_Android_2024.Models.ServicesDTOModel
 
     namespace Backend_Android_2024.Models.DTOModel
     {
-        public class ProductServiceDTO
+        // Service: Show Product Card:
+        public class ProductCartItemDTO
         {
             public int IDSP { get; set; }
             public string TenSP { get; set; }
+            public string Img_Url { get; set; }
             public int GiaBan { get; set; }
-            public int GiaMua { get; set; }
-            public int SoLuong { get; set; }
-            public string MoTa { get; set; }
-            public int IDKichThuoc { get; set; }
 
-            // thương hiệu
-            public int IDThuongHieu { get; set; }
-            public string TenThuongHieu { get; set; }
-
-            // loại sản phẩm
-            public int IDLoai { get; set; }
-            public string TenLoai { get; set; }
-
-            // màu sắc
-            public int IDMau { get; set; }
-            public string TenMau { get; set; }
-
-            // trạng thái sản phẩm
-            public int IDTrangThai { get; set; }
-            public string TenTrangThai { get; set; }
-
-            // nhân viên
-            public int IDNV { get; set; }
-            public string TenNV { get; set; }
         }
     }
 
